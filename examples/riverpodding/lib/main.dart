8031a980af73c5cba416e267b6d57e8492dea4b7
Shaun Wu <65869197+iamshaunwu@users.noreply.github.com>
2023-02-16 20:12:21 +0800

    pv

--- examples/riverpodding/lib/main.dart
+++ examples/riverpodding/lib/main.dart
@@ -9,8 +9,6 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    const myName = 'apple';
-
     return const MaterialApp(
       home: Scaffold(
         body: Center(
